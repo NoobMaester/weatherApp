d7e3800790a8a957ab083ed5b02c836e61b3585b
noobmaester <joshstilezgmail.com>
2023-08-03 20:07:40 +0200

    adding a loading spinner

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ const submit = document.querySelector(".btn");
 
 const apiKey = "abed9c28677b475992280043230208";
 
+const displayLoading = () => {
+  document.querySelector(".loading").style.display = "block";
+  setTimeout(() => {
+    document.querySelector(".loading");
+  }, 3000);
+};
+const hideLoading = () => {
+  document.querySelector(".loading").style.display = "none";
+};
+
 const checkWeather = async (city) => {
+  displayLoading();
   const response = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`
   );
   const data = await response.json();
+  hideLoading();
 
   // console.log(data.current.condition.icon);
   // console.log(data.current.temp_c);
@@ -19,10 +31,11 @@ const checkWeather = async (city) => {
   document.getElementById("icon").src = data.current.condition.icon;
   document.getElementById("condition").innerHTML = data.current.condition.text;
 
-  document.querySelector(".output").style.display = "block"
+  document.querySelector(".output").style.display = "block";
 };
 
 submit.addEventListener("click", (e) => {
   e.preventDefault();
   checkWeather(inputText.value);
+  inputText.value = "";
 });
