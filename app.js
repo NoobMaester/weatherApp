63bc1a06c696afc7c33301de0d0fffa3d828a081
noobmaester <joshstilezgmail.com>
2023-08-03 19:29:35 +0200

    displaying city data

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
+const inputText = document.querySelector(".inputTxt");
+const submit = document.querySelector(".btn");
 
 const apiKey = "abed9c28677b475992280043230208";
-const city = "london"
-const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
 
-const checkWeather = async () => {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
+const checkWeather = async (city) => {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`
+  );
+  const data = await response.json();
 
-    console.log(data.current);
-    console.log(data.current.temp_c);
-    console.log();
-    console.log();
-    console.log();
-}
-checkWeather();
+  // console.log(data.current.condition.icon);
+  // console.log(data.current.temp_c);
+  // console.log(data);
+  // console.log(data.location.name);
+
+  document.getElementById("city").innerHTML = data.location.name;
+  document.getElementById("temp").innerHTML = data.current.temp_c + "ºC";
+  document.getElementById("icon").src = data.current.condition.icon;
+  document.getElementById("condition").innerHTML = data.current.condition.text;
+
+  document.querySelector(".output").style.display = "block"
+};
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  checkWeather(inputText.value);
+});
