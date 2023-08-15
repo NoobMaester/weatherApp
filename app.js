0fa6726f8a55a9b688dab5e11e4d7a64ab1f3baf
noobmaester <joshstilezgmail.com>
2023-08-15 14:03:19 +0200

    error handling with try/catch...

--- app.js
+++ app.js
@@ -5,33 +5,33 @@ const apiKey = "abed9c28677b475992280043230208";
 
 const displayLoading = () => {
   document.querySelector(".loading").style.display = "block";
-  setTimeout(() => {
-    document.querySelector(".loading");
-  }, 3000);
 };
+
 const hideLoading = () => {
   document.querySelector(".loading").style.display = "none";
 };
 
 const checkWeather = async (city) => {
   displayLoading();
-  const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`
-  );
-  const data = await response.json();
-  hideLoading();
+  
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`
+    );
+    const data = await response.json();
+    hideLoading();
+    
+    document.getElementById("city").innerHTML = data.location.name;
+    document.getElementById("temp").innerHTML = data.current.temp_c + "ºC";
+    document.getElementById("icon").src = data.current.condition.icon;
+    document.getElementById("condition").innerHTML =
+      data.current.condition.text;
 
-  // console.log(data.current.condition.icon);
-  // console.log(data.current.temp_c);
-  // console.log(data);
-  // console.log(data.location.name);
+    document.querySelector(".output").style.display = "flex";
 
-  document.getElementById("city").innerHTML = data.location.name;
-  document.getElementById("temp").innerHTML = data.current.temp_c + "ºC";
-  document.getElementById("icon").src = data.current.condition.icon;
-  document.getElementById("condition").innerHTML = data.current.condition.text;
-
-  document.querySelector(".output").style.display = "block";
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 submit.addEventListener("click", (e) => {
